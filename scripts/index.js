a1159fe537cc741d3e32fc80be41081077a0d4d2
jonjack17 <151687987+jonjack17@users.noreply.github.com>
2025-04-23 19:22:01 -0400

    Using fetch() and async/await to get data from API

--- scripts/index.js
+++ scripts/index.js
@@ -3,4 +3,19 @@
 
 const testEl=document.getElementById('test')
 
-testEl.innerHTML = 'test'
+testEl.textContent = 'test'
+
+
+async function getAllData() {
+    const url = "http://localhost:3000/data/1";
+    const response = await fetch(url);
+    
+    const jsonResponse = await response.json();
+    
+    testEl.innerHTML = `${jsonResponse.info}`
+    console.log(typeof jsonResponse)
+}
+
+getAllData()
+
+
