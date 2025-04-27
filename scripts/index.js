a3426cf161ba41c7243500286fef3c61e69212c8
jonjack17 <151687987+jonjack17@users.noreply.github.com>
2025-04-27 18:09:44 -0400

    Installed tailwind and began initial styling

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,17 @@
 
 
-
 const testEl=document.getElementById('test')
 
 testEl.textContent = 'test'
 
 
 async function getAllData() {
-    const url = "http://localhost:3000/data/1";
+    const url = "http://localhost:3000/data/";
     const response = await fetch(url);
     
     const jsonResponse = await response.json();
     
-    testEl.innerHTML = `${jsonResponse.info}`
+    testEl.innerHTML = jsonResponse[0].info
     console.log(typeof jsonResponse)
 }
 
