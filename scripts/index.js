c500c304ba03bec0c15e969d605e47859c1d1af0
jonjack17 <151687987+jonjack17@users.noreply.github.com>
2025-04-28 19:33:02 -0400

    Styling and data processing
    Switched back to vanilla CSS. worked on normalizing data and getting it to show up / process in my html. needed to add an additional await statement, that problem solved!

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,40 @@
 
 
 const testEl=document.getElementById('test')
+const getAllUsersBtn = document.getElementById('get-all-users')
 
 testEl.textContent = 'test'
 
 
-async function getAllData() {
+
+async function displayAllData() {
     const url = "http://localhost:3000/data/";
     const response = await fetch(url);
     
     const jsonResponse = await response.json();
-    
-    testEl.innerHTML = jsonResponse[0].info
-    console.log(typeof jsonResponse)
-}
 
-getAllData()
+    const normalizedResponse = await jsonResponse.map(user => ({
+         
+        id:user.id,
+        name:user.name,
+        email: user.email,
+        info: user.info,
+        fruit: user.fruit
+
+    }))
+
+   return normalizedResponse
+}   
+
+const normalizedResponse = await displayAllData()
+
+
+console.log(normalizedResponse[0].info)
+
+
+// getAllUsersBtn.addEventListener("click", function(e) {
+//     displayAllData()
+   
+// })
 
 
